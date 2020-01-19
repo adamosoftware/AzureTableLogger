3ad761ff453abb51e28fd4503483512bf01eaad7
Adam O'Neil <adamosoftware@gmail.com>
2020-01-19 18:21:35 -0500

    Add project files.

---
+++ AzureTableLogger/Models/ExceptionEntity.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+
+namespace AzureTableLogger.Models
+{
+    public class ExceptionEntity : TableEntity
+    {
+        public ExceptionEntity(string appName, Exception exception)
+        {
+            PartitionKey = appName;
+            RowKey = Guid.NewGuid().ToString();
+            Message = exception.Message;
+            StackTrace = exception.StackTrace;
+        }
+
+        public string MachineName { get; set; }
+        public string UserName { get; set; }
+        public string MethodName { get; set; }
+        public string Message { get; set; }
+        public string StackTrace { get; set; }
+    }
+}
---
+++ AzureTableLogger/TableStorageLogger.cs
@@ -0,0 +1,40 @@
+﻿using AzureTableLogger.Models;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace AzureTableLogger
+{
+    public class TableStorageLogger
+    {
+        private readonly StorageCredentials _credentials;
+        private readonly string _tableName;
+        private readonly string _appName;
+
+        public TableStorageLogger(string accountName, string accountKey, string tableName, string appName)
+        {
+            _credentials = new StorageCredentials(accountName, accountKey);
+            _tableName = tableName;
+            _appName = appName;
+        }
+
+        public async Task WriteAsync(Exception exception, string machineName = null, string userName = null, [CallerMemberName]string methodName = null)
+        {
+            var log = new ExceptionEntity(_appName, exception)
+            {
+                MachineName = machineName,
+                UserName = userName,
+                MethodName = methodName
+            };
+
+            var account = new CloudStorageAccount(_credentials, true);
+            var client = account.CreateCloudTableClient();
+            var table = client.GetTableReference(_tableName);
+            var operation = TableOperation.Insert(log);
+            await table.ExecuteAsync(operation);
+        }
+    }
+}
