66fce3f81e642d8e495dd721a9911f790053e5f0
Adam O'Neil <adamosoftware@gmail.com>
2020-01-19 20:35:05 -0500

    started on sample app

---
+++ SampleApp/Filters/ExceptionFilter.cs
@@ -0,0 +1,26 @@
+﻿using AzureTableLogger;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Threading.Tasks;
+
+namespace SampleApp.Filters
+{
+    public class ExceptionFilter : IAsyncExceptionFilter
+    {
+        private readonly TableStorageLogger _logger;
+
+        public ExceptionFilter(IConfiguration config)
+        {
+            _logger = new TableStorageLogger(config["StorageAccount:Name"], config["StorageAccount:Key"], "Exceptions", "SampleApp");
+        }
+
+        public async Task OnExceptionAsync(ExceptionContext context)
+        {
+            var result = await _logger.WriteAsync(context);
+
+            context.Result = new RedirectResult($"/Exception?key={result.RowKey}");
+        }
+    }
+}
