8015c185301b55a024feb54a265c5f6f44665ec7
Adam O'Neil <adamosoftware@gmail.com>
2020-01-19 20:34:21 -0500

    started on Netcore project

---
+++ AzureTableLogger.Netcore/ExceptionFilter.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Threading.Tasks;
+
+namespace AzureTableLogger
+{
+    public abstract class ExceptionFilterBase : IAsyncExceptionFilter
+    {
+        private readonly TableStorageLogger _logger;
+
+        public ExceptionFilterBase(IConfiguration config)
+        {
+            _logger = CreateLogger(config);
+        }
+
+        protected abstract TableStorageLogger CreateLogger(IConfiguration config);
+
+        protected abstract string GetRedirectUrl(string rowKey);
+
+        public async Task OnExceptionAsync(ExceptionContext context)
+        {
+            var result = await _logger.WriteAsync(context);
+            context.Result = new RedirectResult(GetRedirectUrl(result.RowKey));
+        }
+    }
+}
---
+++ AzureTableLogger.Netcore/Extensions.cs
@@ -0,0 +1,28 @@
+﻿using AzureTableLogger.Models;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureTableLogger.AspNetCore
+{
+    public static class Extensions
+    {
+        public async static Task<ExceptionEntity> WriteAsync(this TableStorageLogger logger, ExceptionContext context)
+        {
+            var log = new ExceptionEntity(logger.AppName, context.Exception)
+            {
+                UserName = context.HttpContext.User.Identity.Name,
+                MethodName = context.HttpContext.Request.Path.Value,
+                QueryString = context.HttpContext.Request.QueryString.Value,
+                HttpMethod = context.HttpContext.Request.Method
+            };
+
+            await logger.AddLogAsync(log);
+
+            return log;
+        }
+
+    }
+}
