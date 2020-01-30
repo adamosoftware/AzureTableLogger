1876b9e8e7a1bb3f4812c16bceeb1fe5aafd422a
Adam ONeil <adamosoftware@gmail.com>
2020-01-30 15:37:40 -0500

    static assets for Exception Dashboard

---
+++ ExceptionDashboard/exceptions.js
@@ -0,0 +1,17 @@
+﻿let excElement = null;
+let excItem = null;
+
+$('.show-exception').on('click', function (ev) {
+    if (excElement != null) {
+        excElement.hide();
+        excItem.removeClass('selected');
+    }
+
+    $(ev.target).addClass('selected');
+
+    var excId = $(ev.target).data('exception-id');
+    excElement = $('#exc-' + excId);
+    excElement.show();
+    excItem = $(ev.target).parents('.exception-item');
+    excItem.addClass('selected');
+});
